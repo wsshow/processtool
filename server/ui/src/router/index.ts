dd5da85937a06b41a854af9d296ef28ef1d0bb4f
ws <wangshang@nj.iscas.ac.cn>
2023-01-11 14:28:29 +0800

    refactor: remove test file

--- server/ui/src/router/index.ts
+++ server/ui/src/router/index.ts
@@ -5,11 +5,6 @@ const routes: Array<RouteRecordRaw> = [
         path: '/',
         name: 'home',
         component: () => import('../views/HomeView.vue')
-    },
-    {
-        path: '/about',
-        name: 'about',
-        component: () => import('../views/AboutView.vue')
     }
 ]
 
--- server/ui/src/views/AboutView.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <button>{{ msg }}</button>
-</template>
-
-<script lang="ts" setup>
-const msg = '123'
-</script>
